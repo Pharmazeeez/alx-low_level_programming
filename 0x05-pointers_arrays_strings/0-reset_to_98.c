914e0bb91fe01cde13bbb57f4da5de1d162d5409
Pharmazeeez <haypharm4real@gmail.com>
2022-04-22 14:03:25 -0700

    my array

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
+
 /**
  * reset_to_98 - reset argument to 98
- * @n: pointer
+ *
+ * @n: pointer to reset t0 98
  * Return: void
  */
-
-void reset_t0_98(int *n);
+void reset_to_98(int *n)
 {
-
 	*n = 98;
-
 }
